5aafe67fb74ebc03bb022c2c297d24da4af15416
Celebeze <kris1337@gmail.com>
2014-11-13 18:00:23 +0100

    initial commit with fresh libGDX project

---
+++ desktop/src/io/github/celebes/sudoku/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.celebes.sudoku.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.celebes.sudoku.SudokuGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SudokuGame(), config);
+	}
+}
